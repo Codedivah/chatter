2b6b4b0df167ecdc0f95469d5c0b2f9bb54a24be
Codedivah <fatimahbadmus04@gmail.com>
2023-07-01 00:23:14 +0100

    update

--- chatter-api/src/main.ts
+++ chatter-api/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { json } from 'express';
 import { AppModule } from './app.module';
+import  "dotenv/config";
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4200;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(json({ limit: '25mb' }));
   app.enableCors();
-  await app.listen(4200);
+  await app.listen(PORT);
 }
 bootstrap();
